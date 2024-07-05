a29d5cf4d4314f7c09628a8479612d36949fce21
avithalker <avithalker@gmail.com>
2024-07-05 15:03:41 +0300

    adding string operations

--- src/main/scala/playground2/stringOps2.scala
+++ src/main/scala/playground2/stringOps2.scala
@@ -2,4 +2,17 @@ package playground2
 
 object stringOps2 extends App {
 
+  val str:String = "Hello, i am learning Scala"
+
+  println(str.charAt(2))
+  println(str.substring(7,11))
+  println(str.split(" ").toList)
+  println(str.startsWith("Avi"))
+  println(str.replace(" ","-"))
+
+  val name="Avi"
+  val age = 12
+  val greeting = s"Hello, my name is $name and i am $age years old"
+  println(greeting)
+
 }
